70a44f4fffe30a7bb05c99d47486ac53cfa62bfb
varadg1 <varad.s.gampawar>
2025-05-07 19:31:09 +0530

    update the changes

--- HelloStudent.java
+++ HelloStudent.java
@@ -1,5 +1,6 @@
 public class HelloStudent {
     public static void main(String[] args) {
         System.out.println("Hello, Student! Welcome to cdac.");
+	System.out.println("Hello, Student! Welcome to ditiss.");
     }
 }
