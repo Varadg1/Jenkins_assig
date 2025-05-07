c7c92a6a9c28125f3f268961965271bb851834a0
varadg1 <varad.s.gampawar>
2025-05-07 18:41:48 +0530

    update the changes

---
+++ sum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to add three numbers 10, 12, and 14 and display the sum
+
+num1=10
+num2=12
+num3=14
+
+sum=$((num1 + num2 + num3))
+
+echo "The sum of $num1, $num2, and $num3 is: $sum"
